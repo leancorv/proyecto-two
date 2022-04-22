f22811b2273bc25942fd5e9cd10381246c9b48fc
Lean <lecorvalan2@gmail.com>
2022-04-22 00:53:59 -0300

    Efecto scroll navbar con javascript

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+// EFECTO SCROLL NAVBAR
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+    let ubicacionActual = window.pageYOffset
+
+    // console.log(ubicacionActual)
+    if( ubicacionPrincipal >= ubicacionActual ) {
+        $nav.style.backgroundColor = "transparent"
+    } else {
+        $nav.style.backgroundColor= "#141414"
+    }
+
+    // ubicacionPrincipal = ubicacionActual
+})
