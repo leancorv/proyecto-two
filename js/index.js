61bdd879fd15d586be3545eb4d49f682a9f2b529
Leandro <lecorvalan2@gmail.com>
2022-05-15 22:54:42 -0300

    Se añade un dropdown para el usuario en navbar

--- js/index.js
+++ js/index.js
@@ -162,4 +162,22 @@ function throttle(cb, delay = 250) {
         shouldWait = false
       }, delay)
     }
-  }
+}
+
+// DROPDOWN EFFECT
+
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+    
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
